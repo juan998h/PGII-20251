f865e8e52e831cb898d9b91f027cd8a3e7786654
juan998h <juane.gutierrezh@uqvirtual.edu.co>
2025-02-28 21:11:14 -0500

    MODIFICACION DE CLASES
    SE MODIFICAN Y SE ELIMINAN ALGUNAS CLAASES DEL CODIGO

--- hotel/src/main/java/co/edu/uniquindio/poo/Habicion.java
+++ hotel/src/main/java/co/edu/uniquindio/poo/Habicion.java
@@ -2,35 +2,16 @@ package co.edu.uniquindio.poo;
 
 public class Habitacion {
     private int numero;
+    private String tipo;
     private double precio;
-    TipoHabitacion tipo;
 
-    public Habitacion(int numero, double precio, TipoHabitacion tipo) {
+    public Habitacion(int numero, String tipo, double precio) {
         this.numero = numero;
-        this.precio = precio;
         this.tipo = tipo;
-    }
-    public int getNumero() {
-        return numero;
-    }
-    public void setNumero(int numero) {
-        this.numero = numero;
-
-    }
-
-    public double getPrecio() {
-        return precio;
-
-    }
-    public void setPrecio(double precio) {
         this.precio = precio;
     }
-    public TipoHabitacion getTipo() {
-        return tipo;
-    }
-    public void setTipo(TipoHabitacion tipo) {
-        this.tipo = tipo;
-    }
 
-
+    public int getNumero() { return numero; }
+    public String getTipo() { return tipo; }
+    public double getPrecio() { return precio; }
 }
--- hotel/src/main/java/co/edu/uniquindio/poo/Hotel.java
+++ hotel/src/main/java/co/edu/uniquindio/poo/Hotel.java
@@ -1,13 +1,51 @@
 package co.edu.uniquindio.poo;
 
-public abstract class Hotel {
+import java.util.ArrayList;
+import java.util.List;
 
-    protected Servicio servicio;
+public class Hotel {
+    private static Hotel instancia;  // Instancia única
+    private List<Habitacion> habitaciones;
+    private List<Cliente> clientes;
+    private List<Reserva> reservas;
 
-    public Hotel() {
-        this.servicio = Servicio.getInstancia();
+    private HotelManager() {  // Constructor privado
+        habitaciones = new ArrayList<>();
+        clientes = new ArrayList<>();
+        reservas = new ArrayList<>();
     }
 
+    public static HotelManager getInstancia() {
+        if (instancia == null) {
+            instancia = new HotelManager();
+        }
+        return instancia;
+    }
 
-    public abstract void TipoHabitacion();
+    public void agregarHabitacion(Habitacion habitacion) {
+        habitaciones.add(habitacion);
+    }
+
+    public void registrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void realizarReserva(Cliente cliente, Habitacion habitacion, String fechaEntrada, String fechaSalida) {
+        Reserva reserva = new Reserva(cliente, habitacion, fechaEntrada, fechaSalida);
+        reservas.add(reserva);
+        cliente.agregarReserva(reserva);
+        System.out.println("Reserva realizada para " + cliente.getNombre());
+    }
+
+    public List<Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
 }
