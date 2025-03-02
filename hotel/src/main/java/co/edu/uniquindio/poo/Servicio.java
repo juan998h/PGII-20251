c29f8b2ef70217db56c8754f1b00a43fbc80dbb4
Alecrack-123 <janneralejandroa.rojasr@uqvirtual.edu.co>
2025-03-01 19:01:45 -0500

    CLASESSSS

---
+++ hotel/src/main/java/co/edu/uniquindio/poo/Servicio.java
@@ -0,0 +1,11 @@
+public abstract class Servicio implements Consumible {
+    protected String nombre;
+    protected double precio;
+
+    public Servicio(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public abstract void consumir();
+}
